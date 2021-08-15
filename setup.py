fa6a0ca1e50f7e98d01af3f44b1489f00fa47b77
conni <cbuschka@gmail.com>
2021-08-15 15:07:50 +0200

    Changed status to production/ stable.

--- setup.py
+++ setup.py
@@ -56,7 +56,7 @@ setuptools.setup(
     zip_safe=False,
     license='MIT License',
     classifiers=[
-      'Development Status :: 3 - Alpha',
+      'Development Status :: 5 - Production/Stable',
       'Intended Audience :: Developers',
       'License :: OSI Approved :: Apache Software License',
       'Operating System :: OS Independent',
