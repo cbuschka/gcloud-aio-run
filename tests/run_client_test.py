b9480f470b131aa751a63d89ccef2737b7b90f02
conni <cbuschka@gmail.com>
2021-08-15 15:23:14 +0200

    Removed usage of deprecated asyncio.coroutine.

--- tests/run_client_test.py
+++ tests/run_client_test.py
@@ -7,10 +7,9 @@ from gcloud.aio.run import RunClient
 
 def async_test(f):
   def wrapper(*args, **kwargs):
-    coro = asyncio.coroutine(f)
-    future = coro(*args, **kwargs)
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(future)
+    result = loop.run_until_complete(f(*args, **kwargs))
+    return result
 
   return wrapper
 
